e98e70ee548ea1b7e1ee738bcabf1d38fd986dfd
Alex Beytuk <alexbeytuk@gmail.com>
2016-10-14 23:47:38 +0300

    Fixed Manifest and Activity

--- app/src/main/java/com/example/alex/facebooklayout/FourthActivity.java
+++ app/src/main/java/com/example/alex/facebooklayout/FourthActivity.java
@@ -3,6 +3,7 @@ package com.example.alex.facebooklayout;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,7 +21,7 @@ public class FourthActivity extends AppCompatActivity {
     public void btnClick(View view) {
         EditText etext = (EditText) findViewById(R.id.editName);
         String userName = etext.getText().toString();
-        if(userName.equals("")){
+        if(TextUtils.isEmpty(userName)){
             printData("No name is entered");
             return;
         }
